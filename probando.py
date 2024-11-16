a90da49f3e10634a5a11103e71d308e4b5de8b8c
Blanca-Portillo <blancaportillo0017@gmail.com>
2024-11-16 14:27:27 -0600

    aja

--- probando.py
+++ probando.py
@@ -1,8 +1,8 @@
 import tkinter as tk
 from tkinter import messagebox
 import sqlite3
-import matplotlib.pyplot as plt
-import pandas as pd
+import matplotlib.pyplot as plt # type: ignore
+import pandas as pd # type: ignore
 import datetime
 from cryptography.fernet import Fernet
 
